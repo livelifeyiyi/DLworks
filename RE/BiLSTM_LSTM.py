ce235d25a0b8820f166a5f155a44d424eefea7cf
livelifeyiyi <xiaoya_0922@126.com>
2019-04-16 23:34:22 +0800

    set for test data

--- RE/BiLSTM_LSTM.py
+++ RE/BiLSTM_LSTM.py
@@ -71,15 +71,15 @@ class DecoderRNN(nn.Module):
 			self.embedding = nn.Embedding(self.embedding_size, self.embedding_dim)
 		self.lstm = nn.LSTM(input_size=self.embedding_dim*2, hidden_size=self.hidden_dim, batch_first=True, num_layers=2, dropout=self.dropout)  # hidden_size, hidden_size)
 		self.entity_embeds = nn.Embedding(self.tag_size, self.hidden_dim)
-		# self.hidden2tag = nn.Linear(self.hidden_dim, self.tag_size)  # out
-		# self.softmax = nn.LogSoftmax(dim=1)
+		self.hidden2tag = nn.Linear(self.hidden_dim, self.tag_size)  # out
+		self.softmax = nn.LogSoftmax(dim=1)
 
 	def forward(self, input, hidden):
 		# output = self.embedding(input).view(-1, self.batch, self.embedding_dim)   # ??? need embedding??
 		output = F.relu(input)
 		output, hidden = self.lstm(output, hidden)
-		# output = self.softmax(self.hidden2tag(output))
-		return output, hidden
+		output_tag = self.softmax(self.hidden2tag(output))
+		return output, output_tag, hidden
 
 	def initHidden(self):
 		# (layers*direction, batch, hidden)
@@ -104,7 +104,7 @@ def timeSince(since, percent):
 teacher_forcing_ratio = 0.5
 
 
-def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, BATCH):  # max_length=MAX_LENGTH
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, BATCH, TEST=False):  # max_length=MAX_LENGTH
 	encoder_hidden = encoder.initHidden_bilstm()
 
 	encoder_optimizer.zero_grad()
@@ -123,53 +123,57 @@ def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, deco
 
 	# input batch
 	encoder_outputs, encoder_hidden = encoder(input_tensor, encoder_hidden)
+	# input_tensor: (batch, seq); encoder_hidden: (layer*direction, batch, hidden_dim//2)
+	# encoder_outputs: (batch, seq, hidden_dim//2*2); encoder_hidden: (layer*direction, batch, hidden_dim//2)
 
 	# decoder_input = torch.tensor([[SOS_token] for i in range(BATCH)], device=device).view(BATCH, 1, 1)
 	# for one-layer
 	# decoder_hidden = (torch.cat((encoder_hidden[0][0], encoder_hidden[0][1]), 1).view(1, BATCH, -1),
 	# 				  torch.cat((encoder_hidden[1][0], encoder_hidden[1][1]), 1).view(1, BATCH, -1))# encoder_hidden
 	# for 2 layer
-	h1 = torch.cat((encoder_hidden[0][0], encoder_hidden[0][1]), 1).view(1, BATCH, -1)
-	h2 = torch.cat((encoder_hidden[0][2], encoder_hidden[0][3]), 1).view(1, BATCH, -1)
+	h1 = torch.cat((encoder_hidden[0][0], encoder_hidden[0][1]), 1).view(1, BATCH, -1)  # concat forward and backward hidden at layer1
+	h2 = torch.cat((encoder_hidden[0][2], encoder_hidden[0][3]), 1).view(1, BATCH, -1)  # layer2
 	c1 = torch.cat((encoder_hidden[1][0], encoder_hidden[1][1]), 1).view(1, BATCH, -1)
 	c2 = torch.cat((encoder_hidden[1][2], encoder_hidden[1][3]), 1).view(1, BATCH, -1)
 	decoder_hidden = (torch.cat((h1, h2), 0),
-					  torch.cat((c1, c2), 0))
+					  torch.cat((c1, c2), 0))  # (layer*direction, batch, hidden_dim)
 
 	decoder_input = encoder_outputs
 
-	decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-	decoder_output_T = decoder_output.transpose(0, 1)
-	target_tensor_T = target_tensor.transpose(0, 1)
+	decoder_output, decoder_output_tag, decoder_hidden = decoder(decoder_input, decoder_hidden)
+	# (batch, seq, hidden_dim)  (layer*direction, batch, hidden_dim)
+	# decoder_output_T = decoder_output_tag.transpose(0, 1)  # (batch, seq, hidden_dim) -- >(seq, batch, hidden_dim)
+	# target_tensor_T = target_tensor.transpose(0, 1)  # (batch, seq) --> (seq, batch)
 	# for i in range(target_length):
-	for j in range(seq_length):  # each word
-		loss += criterion(decoder_output_T[j], target_tensor_T[j])
+	if not TEST:
+		for j in range(target_length):  # each sentence  # seq_length
+			loss += criterion(decoder_output_tag[j], target_tensor[j])
 
-	'''
-	use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-	if use_teacher_forcing:
-		# Teacher forcing: Feed the target as the next input
-		for di in range(target_length):
-			decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-			loss += criterion(decoder_output, target_tensor[di])
-			decoder_input = target_tensor[di]  # Teacher forcing
-	else:
-		# Without teacher forcing: use its own predictions as the next input
-		for di in range(target_length):
-			decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
-			topv, topi = decoder_output.topk(1)
-			decoder_input = topi.squeeze().detach()  # detach from history as input
+		'''
+		use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+		if use_teacher_forcing:
+			# Teacher forcing: Feed the target as the next input
+			for di in range(target_length):
+				decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+				loss += criterion(decoder_output, target_tensor[di])
+				decoder_input = target_tensor[di]  # Teacher forcing
+		else:
+			# Without teacher forcing: use its own predictions as the next input
+			for di in range(target_length):
+				decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+				topv, topi = decoder_output.topk(1)
+				decoder_input = topi.squeeze().detach()  # detach from history as input
+	
+				loss += criterion(decoder_output, target_tensor[di])
+				if decoder_input.item() == EOS_token:
+					break
+		'''
+		loss.backward(retain_graph=True)
 
-			loss += criterion(decoder_output, target_tensor[di])
-			if decoder_input.item() == EOS_token:
-				break
-	'''
-	loss.backward()
+		encoder_optimizer.step()
+		decoder_optimizer.step()
 
-	encoder_optimizer.step()
-	decoder_optimizer.step()
-
-	return loss.item() / float(seq_length), encoder_outputs, decoder_output
+	return loss.item() / float(seq_length), encoder_outputs, decoder_output, decoder_output_tag
 
 
 def trainEpoches(encoder, decoder, criterion, print_every=10, learning_rate=0.001, l2=0.0001):
