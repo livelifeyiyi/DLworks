4f4369baf41f8977b1e40e936d224e9db9103a05
livelifeyiyi <xiaoya_0922@126.com>
2019-07-18 13:57:18 +0800

    preprocess dictionart

--- BERT/preTrainEval/preprocess.py
+++ BERT/preTrainEval/preprocess.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import codecs
 import json
+
+import re
 from tqdm import tqdm
 import ast
 
-file_names_jsonl = []
+file_names_jsonl = ['skill_word_to_entity.jsonl']
 all_words = []
+
+
+def add_words(word, all_words):
+	# / , \u300, \n
+	word = word.replace(' ', '').replace('\n', '').replace('\\u300', '/')
+	if '/' in word or ',' in word:
+		all_words += re.split(r'[/,]', word)
+	elif isinstance(word, str):
+		all_words.append(word)
+	elif isinstance(word, list):
+		all_words += word
+	else:
+		print(word)
+	return all_words
+
 
 for file_name in file_names_jsonl:
 	with codecs.open(file_name, encoding='utf-8', mode='r') as infile:
@@ -13,41 +30,51 @@ for file_name in file_names_jsonl:
 			word = ast.literal_eval(each_line)[0]
 			all_words.append(word)
 
-df_degree = pd.read_csv("degree.csv", header=0)
-all_words += list(df_degree["name"])
+df_degree = pd.read_csv("degrees.csv", header=0)
+for each in df_degree["name"]:
+	all_words = add_words(each, all_words)
+# all_words += list(df_degree["name"])
 del df_degree
 
 df_departments = pd.read_csv("departments.csv", header=0)
-all_words += list(df_departments['name'])
+for each in df_departments['name']:
+	all_words = add_words(each, all_words)
 
 for each in df_departments['aliases']:
-	all_words += each.split(',')
+	all_words = add_words(each, all_words)
+	# if isinstance(each, str):
+	# 	all_words += each.split(',')
 del df_departments
 
 df_functions = pd.read_csv('functions_cluster.csv', header=0)
-all_words += list(df_functions['name'])
+for each in df_functions['name']:
+	all_words = add_words(each, all_words)
 del df_functions
 
-df_unction = pd.read_csv('unction_taxonomy.txt', header=0, sep='\t')
-all_words += list(df_unction['name'])
+df_unction = pd.read_csv('function_taxonomy.txt', header=0, sep='\t')
+for each in df_unction['name']:
+	all_words = add_words(each, all_words)
 del df_unction
 
 df_gsystem = pd.read_csv('gsystem_industries.txt', header=0, sep='\t')
-all_words += [each.split('/') for each in list(df_gsystem['name'])]
+for each in df_gsystem['name']:
+	all_words = add_words(each, all_words)
 del df_gsystem
 
 df_industries = pd.read_csv('industries.csv', header=0)
-all_words += [each.split('/') for each in list(df_industries['name'])]
+for each in df_industries['name']:
+	all_words = add_words(each, all_words)
 del df_industries
 
-csv_filenames = ['majors.csv', 'region.csv', 'schools.csv', 'more_skill_certificates.csv']
+csv_filenames = ['majors.csv', 'region.csv', 'schools.csv', 'skill_certificates.csv']
 for csv_name in csv_filenames:
 	df = pd.read_csv(csv_name, header=0)
-	all_words += list(df['name'])
+	for each in df['name']:
+		all_words = add_words(each, all_words)
 
-with codecs.open('skill_word_to_entity.jsonl', encoding='utf-8', mode='r') as infile:
-	for line_num, each_line in tqdm(enumerate(infile.readlines()), desc='processing file %s' % file_name):
-		all_words += list(json.loads(each_line)[0].keys())
+# with codecs.open('skill_word_to_entity.jsonl', encoding='utf-8', mode='r') as infile:
+# 	for line_num, each_line in tqdm(enumerate(infile.readlines()), desc='processing file %s' % file_name):
+# 		all_words += list(json.loads(each_line)[0].keys())
 
 all_words = list(set(all_words))
 with codecs.open('dictionary.txt', encoding='utf-8', mode='a+') as outfile:
