7ea75480d2718471eb282796ca9a59d9fa3d19bd
xiaoya <xiaoya_0922@126.com>
2019-07-30 10:34:34 +0800

    Update train.py

--- BERT/bertTransformer/train.py
+++ BERT/bertTransformer/train.py
@@ -281,20 +281,32 @@ def train(args, device_id):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("-encoder", default='transformer', type=str,
+	parser.add_argument("--encoder", default='transformer', type=str,
 						choices=['classifier', 'transformer', 'rnn', 'baseline'])
-	parser.add_argument("-mode", default='train', type=str, choices=['train', 'validate', 'test'])
-	parser.add_argument("-do_eval", default='False', action='store_true')
-	parser.add_argument("-bert_data_path", default='C:\\(O_O)!\\learnSth\\PycharmProjects\\python3\\TFgirl\\BERT\\data\\')
-	parser.add_argument("-model_path", default='../models/')
-	parser.add_argument("-result_path", default='../results/cnndm')
-	parser.add_argument("-temp_dir", default='C:\\(O_O)!\\learnSth\\PycharmProjects\\python3\\TFgirl\\BERT\\bert-base-chinese', help='pre-trained bert model')
-	parser.add_argument("-bert_config_path", default='C:\\(O_O)!\\learnSth\\PycharmProjects\\python3\\TFgirl\\BERT\\bert-base-chinese\\bert_config.json')
+	parser.add_argument("--mode", default='train', type=str, choices=['train', 'validate', 'test'])
+	parser.add_argument("--do_eval", default='False', action='store_true')
+	parser.add_argument("--bert_data_path", default='C:\\(O_O)!\\learnSth\\PycharmProjects\\python3\\TFgirl\\BERT\\data\\')
+	parser.add_argument("--model_path", default='../models/')
+	parser.add_argument("--result_path", default='../results/cnndm')
+	parser.add_argument("--temp_dir", default='C:\\(O_O)!\\learnSth\\PycharmProjects\\python3\\TFgirl\\BERT\\bert-base-chinese', help='pre-trained bert model')
+	parser.add_argument("--bert_config_path", default='C:\\(O_O)!\\learnSth\\PycharmProjects\\python3\\TFgirl\\BERT\\bert-base-chinese\\bert_config.json')
 
-	parser.add_argument("-batch_size", default=16, type=int)
-	parser.add_argument("-train_steps", default=1000, type=int)
+	parser.add_argument("--batch_size", default=16, type=int)
+	parser.add_argument("--train_epochs", default=3, type=int)
 
-	parser.add_argument("-polarities_dim", default=3, type=int, help="The dimension of the output classes")
+	parser.add_argument("--max_seq_length", default=512, type=int)
+	parser.add_argument("--lr", default=1, type=float)
+	parser.add_argument("--dropout", default=0.1, type=float)
+	parser.add_argument("--optim", default='adam', type=str)
+	parser.add_argument("--polarities_dim", default=3, type=int, help="The dimension of the output classes")
+
+	parser.add_argument('--visible_gpus', default='1', type=str)
+	parser.add_argument('--gpu_ranks', default='1', type=str)
+	parser.add_argument('--log_file', default='../logs/cnndm.log')
+	parser.add_argument('--dataset', default='')
+	parser.add_argument('--seed', default=666, type=int)
+
+	parser.add_argument("--save_checkpoint_epochs", default=1, type=int)
 
 	parser.add_argument("-use_interval", type=str2bool, nargs='?', const=True, default=True)
 	parser.add_argument("-hidden_size", default=128, type=int)
@@ -305,26 +317,17 @@ if __name__ == '__main__':
 
 	parser.add_argument("-param_init", default=0, type=float)
 	parser.add_argument("-param_init_glorot", type=str2bool, nargs='?', const=True, default=True)
-	parser.add_argument("-dropout", default=0.1, type=float)
-	parser.add_argument("-optim", default='adam', type=str)
-	parser.add_argument("-lr", default=1, type=float)
+
 	parser.add_argument("-beta1", default=0.9, type=float)
 	parser.add_argument("-beta2", default=0.999, type=float)
 	parser.add_argument("-decay_method", default='', type=str)
 	parser.add_argument("-warmup_steps", default=8000, type=int)
 	parser.add_argument("-max_grad_norm", default=0, type=float)
 
-	parser.add_argument("-save_checkpoint_steps", default=5, type=int)
 	parser.add_argument("-accum_count", default=1, type=int)
 	parser.add_argument("-world_size", default=1, type=int)
 	parser.add_argument("-report_every", default=1, type=int)
 	parser.add_argument("-recall_eval", type=str2bool, nargs='?', const=True, default=False)
-
-	parser.add_argument('-visible_gpus', default='-1', type=str)
-	parser.add_argument('-gpu_ranks', default='0', type=str)
-	parser.add_argument('-log_file', default='../logs/cnndm.log')
-	parser.add_argument('-dataset', default='')
-	parser.add_argument('-seed', default=666, type=int)
 
 	parser.add_argument("-test_all", type=str2bool, nargs='?', const=True, default=False)
 	parser.add_argument("-test_from", default='')
